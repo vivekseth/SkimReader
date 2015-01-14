328c74137363d2acb5ac3105e45495f71e610154
Vivek Seth <Vivekseth.m@gmail.com>
2015-01-14 18:13:21 -0500

    add back pods

---
+++ Pods/Target Support Files/Pods-SkimReader-AFNetworking/Pods-SkimReader-AFNetworking-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SkimReader_AFNetworking : NSObject
+@end
+@implementation PodsDummy_Pods_SkimReader_AFNetworking
+@end
---
+++ Pods/Target Support Files/Pods-SkimReader-KFEpubKit/Pods-SkimReader-KFEpubKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SkimReader_KFEpubKit : NSObject
+@end
+@implementation PodsDummy_Pods_SkimReader_KFEpubKit
+@end
